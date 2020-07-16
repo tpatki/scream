3014b71312c0377e9a19e87b99a3ae0a006d7276
Benjamin Hillman <bhillma@sandia.gov>
2020-07-16 13:29:52 -0600

    Add simple test for RRTMGP I/O

--- components/scream/src/physics/rrtmgp/tests/rrtmgp_tests.cpp
+++ components/scream/src/physics/rrtmgp/tests/rrtmgp_tests.cpp
@@ -1,11 +1,28 @@
 #include "catch2/catch.hpp"
 #include "physics/rrtmgp/rrtmgp.hpp"
-
+#include "netcdf.h"
 namespace {
 
-TEST_CASE("rrtmgp", "stub") {
-  int val = scream::rrtmgp::rrtmgp_stub();
-  REQUIRE(val == 42);
-}
+    TEST_CASE("rrtmgp", "stub") {
+      int val = scream::rrtmgp::rrtmgp_stub();
+      REQUIRE(val == 42);
+    }
+
+    TEST_CASE("rrtmgp_read_netcdf", "") {
+
+        /* Try to read absorption coefficient data */
+        int ncid, varid;
+        int retval;
+        
+        // Check that we can open aborption coefficient files
+        const char *coefficients_file_sw = "./data/rrtmgp-data-sw-g224-2018-12-04.nc";
+        const char *coefficients_file_lw = "./data/rrtmgp-data-lw-g256-2018-12-04.nc";
+        REQUIRE(nc_open(coefficients_file_sw, NC_NOWRITE, &ncid) == NC_NOERR);
+
+        // Try reading something relevant
+
+        // If we got this far, we won
+        REQUIRE(1 == 1);
+    }
 
 } // empty namespace
