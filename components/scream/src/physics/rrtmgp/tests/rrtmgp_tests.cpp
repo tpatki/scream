1413b3280e09aa331f1748fe31ec3384c848efff
Benjamin Hillman <bhillma@sandia.gov>
2020-07-23 15:53:30 -0600

    Read in example atmosphere for RRTMGP

--- components/scream/src/physics/rrtmgp/tests/rrtmgp_tests.cpp
+++ components/scream/src/physics/rrtmgp/tests/rrtmgp_tests.cpp
@@ -1,6 +1,8 @@
 #include "catch2/catch.hpp"
 #include "physics/rrtmgp/scream_rrtmgp_interface.hpp"
 #include "netcdf.h"
+#include "mo_gas_concentrations.h"
+#include "mo_garand_atmos_io.h"
 namespace {
 
     TEST_CASE("rrtmgp_read_netcdf", "") {
@@ -28,6 +30,34 @@ namespace {
         // differences.
         REQUIRE(int(scream::rrtmgp::k_dist_sw.press_ref(1)) == 109663);
         REQUIRE(int(scream::rrtmgp::k_dist_sw.press_ref(size(scream::rrtmgp::k_dist_sw.press_ref,1))) == 1);
+    }
+
+    TEST_CASE("rrtmgp_run", "") {
+        // Initialize absorption coefficients
+        scream::rrtmgp::rrtmgp_initialize();
+
+        // Read in dummy Garand atmosphere
+        std::string inputfile = "./data/rrtmgp-allsky.nc";
+        real2d p_lay;
+        real2d t_lay;
+        real2d p_lev;
+        real2d t_lev;
+        real2d col_dry;
+        GasConcs gas_concs;
+        int ncol = 1;
+        read_atmos(inputfile, p_lay, t_lay, p_lev, t_lev, gas_concs, col_dry, ncol);
+
+        // Check that data was loaded properly; just sanity check a few values
+        REQUIRE(int(p_lay(1,1)) == 100933);
+        REQUIRE(int(p_lay(1,size(p_lay))) == 19);
+
+        // Run RRTMGP code on dummy atmosphere
+ 
+        // Write fluxes to file
+
+        // Check fluxes against reference; note that input file contains reference fluxes
+
+        // ALTERNATIVELY: create a single or two-layer atmosphere to do a dummy calc
     }
 
 } // empty namespace
