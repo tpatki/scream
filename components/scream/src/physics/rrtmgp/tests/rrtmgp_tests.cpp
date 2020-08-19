4030fe2aceacac5169d234a4f521aaabfad7480e
Benjamin Hillman <bhillma@sandia.gov>
2020-08-19 12:38:22 -0600

    Move example atmos to new source file

--- components/scream/src/physics/rrtmgp/tests/rrtmgp_tests.cpp
+++ components/scream/src/physics/rrtmgp/tests/rrtmgp_tests.cpp
@@ -2,30 +2,13 @@
 #include <cmath>
 #include "catch2/catch.hpp"
 #include "physics/rrtmgp/scream_rrtmgp_interface.hpp"
+#include "rrtmgp_dummy_atmos.hpp"
 #include "netcdf.h"
 #include "mo_gas_concentrations.h"
-#include "mo_garand_atmos_io.h"
 #include "mo_fluxes.h"
 #include "mo_cloud_optics.h"
 #include "FortranIntrinsics.h"
 namespace {
-
-    void dummy_clouds(
-            CloudOptics &cloud_optics, real2d &p_lay, real2d &t_lay, 
-            real2d &lwp, real2d &iwp, real2d &rel, real2d &rei
-        );
-
-    void dummy_atmos(
-            std::string inputfile,
-            int ncol, real2d &p_lay, real2d &t_lay, real2d &p_lev, real2d &t_lev, GasConcs &gas_concs, real2d &col_dry, 
-            real2d &sfc_alb_dir, real2d &sfc_alb_dif, real1d &mu0,
-            real2d &lwp, real2d &iwp, real2d &rel, real2d &rei
-        );
-
-    void read_fluxes(
-            std::string inputfile, 
-            real2d &sw_flux_up, real2d &sw_flux_dn, real2d &sw_flux_dn_dir,
-            real2d &lw_flux_up, real2d &lw_flux_dn);
 
     // TODO: use YAKL intrinsics for this; this won't work on the GPU
     bool all_equals(real2d &arr1, real2d &arr2) {
@@ -62,22 +45,6 @@ namespace {
         return minval;
     }
 
-    TEST_CASE("rrtmgp_read_netcdf", "") {
-
-        /* Try to read absorption coefficient data */
-        int ncid;
-        
-        // Check that we can open aborption coefficient files
-        const char *coefficients_file_sw = "./data/rrtmgp-data-sw-g224-2018-12-04.nc";
-        const char *coefficients_file_lw = "./data/rrtmgp-data-lw-g256-2018-12-04.nc";
-        REQUIRE(nc_open(coefficients_file_sw, NC_NOWRITE, &ncid) == NC_NOERR);
-
-        // Try reading something relevant
-
-        // If we got this far, we won
-        REQUIRE(1 == 1);
-    }
-
     TEST_CASE("rrtmgp_init", "") {
         scream::rrtmgp::rrtmgp_initialize();
         REQUIRE(1 == 1);
@@ -92,6 +59,20 @@ namespace {
     TEST_CASE("rrtmgp_run", "") {
         // Initialize absorption coefficients
         scream::rrtmgp::rrtmgp_initialize();
+
+        // Get reference fluxes from input file; do this here so we can get ncol dimension
+        std::string inputfile = "./data/rrtmgp-allsky.nc";
+        real2d sw_flux_up_ref;
+        real2d sw_flux_dn_ref;
+        real2d sw_flux_dn_dir_ref;
+        real2d lw_flux_up_ref;
+        real2d lw_flux_dn_ref;
+        rrtmgp::read_fluxes(inputfile, sw_flux_up_ref, sw_flux_dn_ref, sw_flux_dn_dir_ref, lw_flux_up_ref, lw_flux_dn_ref );
+
+        // Get dimension sizes
+        int ncol = sw_flux_up_ref.dimension[0];
+        int nlev = sw_flux_up_ref.dimension[1];
+        int nlay = nlev - 1;
 
         // Read in dummy Garand atmosphere; if this were an actual model simulation, 
         // these would be passed as inputs to the driver
@@ -113,26 +94,20 @@ namespace {
         real2d iwp;
         real2d rel;
         real2d rei;
-        int ncol = 128;
-        std::string inputfile = "./data/rrtmgp-allsky.nc";
-        dummy_atmos(
+        rrtmgp::dummy_atmos(
                 inputfile, ncol, p_lay, t_lay, p_lev, t_lev, gas_concs, col_dry, 
                 sfc_alb_dir, sfc_alb_dif, mu0,
                 lwp, iwp, rel, rei
             );
 
-        // Get dummy cloud PHYSICAL properties. Note that this function call
-        // needs the CloudOptics object only because it uses the min and max
-        // valid values from the lookup tables for liquid and ice water path to
-        // create a dummy atmosphere.
-        dummy_clouds(scream::rrtmgp::cloud_optics_sw, p_lay, t_lay, lwp, iwp, rel, rei);
+        // Check that data has the shape we expect
+        REQUIRE(p_lay.dimension[0] == ncol);
+        REQUIRE(p_lay.dimension[1] == nlay);
+        REQUIRE(p_lev.dimension[1] == nlev);
 
         // Check that data was loaded properly; just sanity check a few values
         REQUIRE(int(p_lay(1,1)) == 100933);
         REQUIRE(int(p_lay(1,p_lay.dimension[1])) == 19);
-
-        // Get dimension sizes
-        int nlay = p_lay.dimension[1];
 
         // Setup flux outputs; In a real model run, the fluxes would be
         // input/outputs into the driver (persisting between calls), and
@@ -155,14 +130,6 @@ namespace {
                 sw_flux_up, sw_flux_dn, sw_flux_dn_dir,
                 lw_flux_up, lw_flux_dn);
  
-        // Check fluxes against reference; note that input file contains reference fluxes
-        real2d sw_flux_up_ref ("sw_flux_up_ref " ,ncol,nlay+1);
-        real2d sw_flux_dn_ref  ("sw_flux_dn_ref " ,ncol,nlay+1);
-        real2d sw_flux_dn_dir_ref ("sw_flux_dn_dir_ref ",ncol,nlay+1);
-        real2d lw_flux_up_ref ("lw_flux_up_ref " ,ncol,nlay+1);
-        real2d lw_flux_dn_ref  ("lw_flux_dn_ref " ,ncol,nlay+1);
-        read_fluxes(inputfile, sw_flux_up_ref, sw_flux_dn_ref, sw_flux_dn_dir_ref, lw_flux_up_ref, lw_flux_dn_ref );
-
         // Check values
         REQUIRE(all_equals(sw_flux_up_ref    , sw_flux_up    ));
         REQUIRE(all_equals(sw_flux_dn_ref    , sw_flux_dn    ));
@@ -171,91 +138,6 @@ namespace {
         REQUIRE(all_equals(lw_flux_dn_ref    , lw_flux_dn    ));
 
         // ALTERNATIVELY: create a single or two-layer atmosphere to do a dummy calc
-    }
-
-    void dummy_atmos(
-            std::string inputfile, 
-            int ncol, real2d &p_lay, real2d &t_lay, real2d &p_lev, real2d &t_lev, GasConcs &gas_concs, real2d &col_dry, 
-            real2d &sfc_alb_dir, real2d &sfc_alb_dif, real1d &mu0,
-            real2d &lwp, real2d &iwp, real2d &rel, real2d &rei) {
-
-        read_atmos(inputfile, p_lay, t_lay, p_lev, t_lev, gas_concs, col_dry, ncol);
-
-        // Setup boundary conditions, solar zenith angle, etc
-        // NOTE: this stuff would come from the model in a real run
-        int nbndsw = scream::rrtmgp::k_dist_sw.get_nband();
-        sfc_alb_dir = real2d("sfc_alb_dir", nbndsw, ncol);
-        sfc_alb_dif = real2d("sfc_alb_dif", nbndsw, ncol);
-
-        // Ocean-ish values for surface albedos, just for example
-        memset(sfc_alb_dir , 0.06_wp );
-        memset(sfc_alb_dif , 0.06_wp );
-
-        // Pick a solar zenith angle; this should come from the model
-        mu0 = real1d("mu0", ncol);
-        memset(mu0, 0.86_wp );
-
-    }
-
-    void dummy_clouds(
-            CloudOptics &cloud_optics, real2d &p_lay, real2d &t_lay, 
-            real2d &lwp, real2d &iwp, real2d &rel, real2d &rei) {
-
-        // Problem sizes
-        int ncol = t_lay.dimension[0];
-        int nlay = t_lay.dimension[1];
-
-        REQUIRE(ncol == 128);
-
-        // Generate some fake liquid and ice water data. We pick values to be midway between
-        // the min and max of the valid lookup table values for effective radii
-        real rel_val = 0.5 * (cloud_optics.get_min_radius_liq() + cloud_optics.get_max_radius_liq());
-        real rei_val = 0.5 * (cloud_optics.get_min_radius_ice() + cloud_optics.get_max_radius_ice());
-
-        // Restrict clouds to troposphere (> 100 hPa = 100*100 Pa) and not very close to the ground (< 900 hPa), and
-        // put them in 2/3 of the columns since that's roughly the total cloudiness of earth.
-        // Set sane values for liquid and ice water path.
-        rel = real2d("rel", ncol, nlay);
-        rei = real2d("rei", ncol, nlay);
-        lwp = real2d("lwp", ncol, nlay);
-        iwp = real2d("iwp", ncol, nlay);
-        real2d cloud_mask("cloud_mask", ncol, nlay);
-        parallel_for( Bounds<2>(nlay,ncol) , YAKL_LAMBDA (int ilay, int icol) {
-            cloud_mask(icol,ilay) = p_lay(icol,ilay) > 100._wp * 100._wp && p_lay(icol,ilay) < 900._wp * 100._wp && mod(icol, 3) != 0;
-            // Ice and liquid will overlap in a few layers
-            lwp(icol,ilay) = merge(10._wp,  0._wp, cloud_mask(icol,ilay) && t_lay(icol,ilay) > 263._wp);
-            iwp(icol,ilay) = merge(10._wp,  0._wp, cloud_mask(icol,ilay) && t_lay(icol,ilay) < 273._wp);
-            rel(icol,ilay) = merge(rel_val, 0._wp, lwp(icol,ilay) > 0._wp);
-            rei(icol,ilay) = merge(rei_val, 0._wp, iwp(icol,ilay) > 0._wp);
-        });
-    }
-
-
-    // Function to read fluxes from input file so we can compare our answers against the reference
-    void read_fluxes(
-            std::string inputfile, 
-            real2d &sw_flux_up, real2d &sw_flux_dn, real2d &sw_flux_dn_dir,
-            real2d &lw_flux_up, real2d &lw_flux_dn) {
-
-        // Initialize netcdf reader
-        yakl::SimpleNetCDF io;
-        io.open(inputfile, yakl::NETCDF_MODE_READ);
-
-        // Initialize arrays to hold fluxes
-        int nlev = io.getDimSize("lev");
-        int ncol = io.getDimSize("col_flx");
-        sw_flux_up = real2d("sw_flux_up", ncol, nlev);
-        sw_flux_dn = real2d("sw_flux_dn", ncol, nlev);
-        sw_flux_dn_dir = real2d("sw_flux_dn_dir", ncol, nlev);
-        lw_flux_up = real2d("lw_flux_up", ncol, nlev);
-        lw_flux_dn = real2d("lw_flux_dn", ncol, nlev);
-
-        // Read data
-        io.read(sw_flux_up, "sw_flux_up_result");
-        io.read(sw_flux_dn, "sw_flux_dn_result");
-        io.read(sw_flux_dn_dir, "sw_flux_dir_result");
-        io.read(lw_flux_up, "lw_flux_up_result");
-        io.read(lw_flux_dn, "lw_flux_dn_result");
     }
    
 } // empty namespace
