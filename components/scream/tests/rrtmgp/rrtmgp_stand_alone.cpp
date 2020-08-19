c4adc47f2b204f9c9a24de79ddb35f2ab3cfe982
Benjamin Hillman <bhillma@sandia.gov>
2020-08-19 13:18:34 -0600

    Run RRTMGP example problem in rrtmgp_stand_alone

--- components/scream/tests/rrtmgp/rrtmgp_stand_alone.cpp
+++ components/scream/tests/rrtmgp/rrtmgp_stand_alone.cpp
@@ -1,15 +1,21 @@
+#include <iostream>
+#include <cmath>
 #include <catch2/catch.hpp>
 
 #include "control/atmosphere_driver.hpp"
-
 #include "physics/rrtmgp/atmosphere_radiation.hpp"
 #include "physics/rrtmgp/scream_rrtmgp_interface.hpp"
 #include "physics/share/physics_only_grids_manager.hpp"
-
 #include "share/atm_process/atmosphere_process.hpp"
-
 #include "ekat/ekat_parse_yaml_file.hpp"
 #include "ekat/ekat.hpp"
+#include "netcdf.h"
+#include "mo_gas_concentrations.h"
+#include "mo_garand_atmos_io.h"
+#include "mo_fluxes.h"
+#include "mo_cloud_optics.h"
+#include "FortranIntrinsics.h"
+#include "rrtmgp_test_utils.hpp"
 
 /*
  * This will eventually contain a standalone test for the RRTMGP driver
@@ -53,14 +59,77 @@ namespace scream {
         ad.run(300.0);
         ad.finalize();
 
+        // Setup for standalone (dummy) problem
+
+        // Get reference fluxes from input file; do this here so we can get ncol dimension
+        std::string inputfile = "./data/rrtmgp-allsky.nc";
+        real2d sw_flux_up_ref;
+        real2d sw_flux_dn_ref;
+        real2d sw_flux_dn_dir_ref;
+        real2d lw_flux_up_ref;
+        real2d lw_flux_dn_ref;
+        rrtmgpTest::read_fluxes(inputfile, sw_flux_up_ref, sw_flux_dn_ref, sw_flux_dn_dir_ref, lw_flux_up_ref, lw_flux_dn_ref );
+
+        // Get dimension sizes
+        int ncol = sw_flux_up_ref.dimension[0];
+        int nlev = sw_flux_up_ref.dimension[1];
+        int nlay = nlev - 1;
+
+        // Read in dummy Garand atmosphere; if this were an actual model simulation,
+        // these would be passed as inputs to the driver
+        // NOTE: set ncol to size of col_flx dimension in the input file. This is so
+        // that we can compare to the reference data provided in that file. Note that
+        // this will copy the first column of the input data (the first profile) ncol
+        // times. We will then fill some fraction of these columns with clouds for
+        // the test problem.
+        real2d p_lay;
+        real2d t_lay;
+        real2d p_lev;
+        real2d t_lev;
+        real2d col_dry;
+        GasConcs gas_concs;
+        real2d sfc_alb_dir;
+        real2d sfc_alb_dif;
+        real1d mu0;
+        real2d lwp;
+        real2d iwp;
+        real2d rel;
+        real2d rei;
+        rrtmgpTest::dummy_atmos(
+            inputfile, ncol, p_lay, t_lay, p_lev, t_lev, gas_concs, col_dry,
+            sfc_alb_dir, sfc_alb_dif, mu0,
+            lwp, iwp, rel, rei
+        );
+
+        // Setup flux outputs; In a real model run, the fluxes would be
+        // input/outputs into the driver (persisting between calls), and
+        // we would just have to setup the pointers to them in the
+        // FluxesBroadband object
+        real2d sw_flux_up ("sw_flux_up" ,ncol,nlay+1);
+        real2d sw_flux_dn ("sw_flux_dn" ,ncol,nlay+1);
+        real2d sw_flux_dn_dir("sw_flux_dn_dir",ncol,nlay+1);
+        real2d lw_flux_up ("lw_flux_up" ,ncol,nlay+1);
+        real2d lw_flux_dn ("lw_flux_dn" ,ncol,nlay+1);
+
         // Run RRTMGP standalone codes and compare with AD run
         // Do something interesting here...
         // NOTE: these will get replaced with AD stuff that handles these
-        //rrtmgp::rrtmgp_init();
-        //rrtmgp::rrtmgp_main();
+        rrtmgp::rrtmgp_initialize();
+        rrtmgp::rrtmgp_main(
+            p_lay, t_lay, p_lev, t_lev, gas_concs, col_dry,
+            sfc_alb_dir, sfc_alb_dif, mu0,
+            lwp, iwp, rel, rei,
+            sw_flux_up, sw_flux_dn, sw_flux_dn_dir,
+            lw_flux_up, lw_flux_dn
+        );
         //rrtmgp::rrtmgp_finalize();
 
-        // If we got here, we were able to run the above code
-        REQUIRE(true);
+        // Check values
+        REQUIRE(rrtmgpTest::all_equals(sw_flux_up_ref    , sw_flux_up    ));
+        REQUIRE(rrtmgpTest::all_equals(sw_flux_dn_ref    , sw_flux_dn    ));
+        REQUIRE(rrtmgpTest::all_equals(sw_flux_dn_dir_ref, sw_flux_dn_dir));
+        REQUIRE(rrtmgpTest::all_equals(lw_flux_up_ref    , lw_flux_up    ));
+        REQUIRE(rrtmgpTest::all_equals(lw_flux_dn_ref    , lw_flux_dn    ));
+
     }
 }
